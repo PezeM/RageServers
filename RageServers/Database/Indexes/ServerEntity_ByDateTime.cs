827e09bc32203b879e62f8a71b504c9eb0c8759e
PezeM <pezemos@gmail.com>
2018-11-28 18:23:08 +0100

    Added index for searching by datetime

---
+++ RageServers/Database/Indexes/ServerEntity_ByDateTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RageServers.Entity;
+using Raven.Client.Documents.Indexes;
+
+namespace RageServers.Database.Indexes
+{
+    public class ServerEntity_ByDateTime : AbstractIndexCreationTask<ServerEntity>
+    {
+        public ServerEntity_ByDateTime()
+        {
+            Map = servers =>
+                from server in servers
+                select new
+                {
+                    server.Datetime
+                };
+        }
+    }
+}
