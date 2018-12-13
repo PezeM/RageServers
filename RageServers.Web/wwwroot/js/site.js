28c936a495fdcbd107103b9b265968001fb03d4b
PezeM <pezemos@gmail.com>
2018-12-14 00:39:07 +0100

    Added scrolling to the top after server from nav is clicked

--- RageServers.Web/wwwroot/js/site.js
+++ RageServers.Web/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Scroll page to the top if the server info on list is clicked
 
-// Write your JavaScript code.
+$("a.submit-link").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+});
